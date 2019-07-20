6820de8a86fed5bdd66aa5d482fabad5d8ade652
1622184228 <19971216.123wxm>
2019-07-20 17:54:36 +0800

    Initial commit

---
+++ src/cn/icloudit/Demo01.java
@@ -0,0 +1,24 @@
+package cn.icloudit;
+
+public class Demo01 {
+
+
+        public static void main(String[] args) {
+            for (int i=2;i<1000;i++){
+                boolean flag = true;
+
+                for (int j = 2 ; j < i - 1 ; j++){
+                    if (i%j == 0){
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag){
+                    System.out.println(i);
+                }
+
+
+            }
+        }
+    }
+
---
+++ src/cn/icloudit/Demo02.java
@@ -0,0 +1,10 @@
+package cn.icloudit;
+
+public class Demo02 {
+    public static void main(String[] args) {
+        boolean[] isPrime = new boolean[100];
+        for (int i=2;i<isPrime.length;i++){
+            isPrime[i] = true;
+        }
+    }
+}
